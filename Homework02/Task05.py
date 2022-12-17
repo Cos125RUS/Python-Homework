01a5adf110349114583f273c085a51864fe6f15e
Valerii_Panov <val250640@mail.ru>
2022-12-18 00:18:17 +1000

    HW02Task04

---
+++ Homework02/Task05.py
@@ -0,0 +1,21 @@
+#Реализуйте алгоритм перемешивания списка. (рандомно поменять местами элементы списка между собой)
+
+import random
+
+n = int(input('N = '))
+list = []
+sum = 0
+
+for i in range(1, n+1):
+    list.append(i)
+
+print(list)
+
+for j in range(0,500):
+    for i in range(0,n):
+        index = random.randrange(0, n-1)
+        change = list[i]
+        list[i] = list[index]
+        list[index] = change
+
+print(list)
