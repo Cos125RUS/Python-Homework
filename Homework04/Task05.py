10276e323726ae985bfefc454880eeee65836526
Valerii_Panov <114232405+Cos125RUS@users.noreply.github.com>
2022-12-26 10:20:43 +1000

    Update Task05.py
    HW04

--- Homework04/Task05.py
+++ Homework04/Task05.py
@@ -5,7 +5,6 @@ with open('file1.txt', 'r') as file:
     arg = file.read().replace(' = 0', '').replace(' ', '').split('+')
 with open('file2.txt', 'r') as file:
     arg += file.read().replace(' = 0', '').replace(' ', '').split('+')
-
 dic = {}
 for degree in arg:
     if degree.count('^'):
@@ -18,21 +17,13 @@ for degree in arg:
         dic[key] = [degree.replace('*x','').replace('^','').replace(key,'')]
     else:
         dic[key] += [degree.replace('*x','').replace('^','').replace(key,'')]
-
 formula = ""
 for i in dic.keys():
     sum = 0
     for j in dic[i]:
         sum += int(j)
-    if int(i) > 0:
-        if sum > 1:
-            formula += str(sum) + '*x'
-        if int(i) > 1:
-            formula += '^' + i + ' + '
-        else:
-            formula += ' + '
-    else:
-        formula += str(sum) + ' = 0'
-
+    formula += str(sum) + '*x^' + i + ' + '
+formula += ' = 0'
+formula = formula.replace('x^1', 'x').replace('*x^0', '').replace(' 1*', ' ').replace('+  +', '+').replace('+  ', '')
 with open('res.txt', 'w') as file:
     file.write(formula)
