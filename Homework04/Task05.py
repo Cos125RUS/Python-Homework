4c22d525cb5e95b1733c917eb7a02984733cc273
Valerii_Panov <val250640@mail.ru>
2022-12-26 16:42:50 +1000

    HW04

--- Homework04/Task05.py
+++ Homework04/Task05.py
@@ -1,10 +1,9 @@
 # Даны два файла, в каждом из которых находится запись многочлена.
 # Задача - сформировать файл, содержащий сумму многочленов.
 
-with open('file1.txt', 'r') as file:
-    arg = file.read().replace(' = 0', '').replace(' ', '').split('+')
-with open('file2.txt', 'r') as file:
-    arg += file.read().replace(' = 0', '').replace(' ', '').split('+')
+with open('file1.txt', 'r') as file1:
+    with open('file2.txt', 'r') as file2:
+        arg = (file1.read() + ' + ' + file2.read()).replace(' = 0', '').replace(' ', '').split('+')
 dic = {}
 for degree in arg:
     q = (degree + ' ').replace('*x^', ' ').replace('x^', '1 ').replace('*x', ' 1').replace('x', '1 1').split()
@@ -20,7 +19,6 @@ for i in dic.keys():
     for j in dic[i]:
         sum += int(j)
     formula += str(sum) + '*x^' + str(i) + ' + '
-formula += ' = 0'
 formula = formula.replace('x^1', 'x').replace('*x^0', '').replace(' 1*', ' ').replace('+  +', '+').replace('+  ', '')
 with open('res.txt', 'w') as file:
-    file.write(formula)
+    file.write(formula + ' = 0')
