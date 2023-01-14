75701495ae9dee28e7706c34fd09ecfb901d3f6d
Valerii_Panov <val250640@mail.ru>
2023-01-15 01:32:46 +1000

    Task03(pvp with check)

--- Homework05/Task03.py
+++ Homework05/Task03.py
@@ -1,14 +1,13 @@
 # Создайте программу для игры в ""Крестики-нолики"".
 
 def change_field(field, chois, symbol):
-    chois = list(map(lambda x: x-1, (map(int, (chois.split('.'))))))
+    # chois = list(map(lambda x: x-1, (map(int, (chois.split('.'))))))
     field[chois[0]][chois[1]] = symbol
     flag = 0
 
     for i in range(len(field)):
         if field[i].count(symbol) == 3:
             flag = 1
-
 
     f = list(zip(field[0], field[1], field[2]))
     for i in range(len(f)):
@@ -36,6 +35,15 @@ def change_field(field, chois, symbol):
     else:
         return 0
 
+
+def check(field):
+    if field == '_':
+        return True
+    else:
+        print('Error!')
+        return False
+
+
 field = ['_', '_', '_'], ['_', '_', '_'], ['_', '_', '_']
 player1 = 'X'
 player2 = 'O'
@@ -44,11 +52,17 @@ win = 0
 counter = 0
 
 while win == 0:
+    factorization = False
     move = move%2 + 1
     for i in range(len(field)):
         print(*field[i])
     print()
-    chois = input(f"Player {move} chois: ")
+    # chois = input(f"Player {move} chois: ")
+    # chois = list(map(lambda x: x - 1, (map(int, (input(f"Player {move} chois: ").split('.'))))))
+    # print(chois)
+    while not factorization:
+        chois = list(map(lambda x: x - 1, (map(int, (input(f"Player {move} chois: ").split('.'))))))
+        factorization = check(field[chois[0]][chois[1]])
     win = change_field(field, chois, player1 if move == 1 else player2)
     if counter == 9:
         print('Draw')
@@ -56,6 +70,8 @@ while win == 0:
 for i in range(len(field)):
     print(*field[i])
 print(f'\nPlayer {move} WIN!')
+
+
 
 # while True:
 #     print('Для игры с другим человеком нажми 1\nДля игры с ботом - 2')
